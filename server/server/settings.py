7133b8cdd08cc83e16221fbc7e06766479d73004
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2022-04-25 17:55:11 +0200

    First endpoint server side

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kb.apps.KbConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
