5de14f1cc3cd2af82f85fe5632cba66d3629fe80
Vlad Makarov <v.makarov@student.tudelft.nl>
2022-04-28 12:53:49 +0200

    Add example call to an API

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { fetchExampleApiCall } from './ApiCalls';
 
+// This code uses functional components, you could use classes instead but they're
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [result, setResult] = useState("");
+
+    function getResult() {
+        fetchExampleApiCall().then(res => {
+            setResult(res)
+        })
+    }
+
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Press the button below to call the api:
+            </p>
+                <button onClick={getResult}>Call it </button>
+            <p>Result: {result} </p>
+        </header>
+        </div>
+    );
 }
 
 export default App;
