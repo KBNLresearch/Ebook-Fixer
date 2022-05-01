26330c881d8de3eb169fb0ac5e3b191e5e8cad4a
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2022-05-01 13:23:32 +0200

    Flake8 style check fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env() # reading .env file
+environ.Env.read_env()  # reading .env file
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
