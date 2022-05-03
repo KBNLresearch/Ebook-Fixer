a970549fb486fb17b52f5020ad776c0c2ded470b
Filip Nguyen Duc <H.C.NguyenDuc@student.tudelft.nl>
2022-05-03 18:17:59 +0200

    Adds skeleton for download and upload endpoints

--- server/ebooks/models.py
+++ server/ebooks/models.py
@@ -8,6 +8,12 @@ from django.db import models
 class Ebook(models.Model):
     # The use of a UUID is mandatory for epub files
     # Include option editable=False ??
-    uuid = models.CharField(primary_key=True, max_length=100, default=uuid.uuid4, unique=True)
+    uuid = models.CharField(primary_key=True, max_length=100, default="DEFAULT", unique=True, editable=False)
     epub3_path = models.CharField(max_length=50)
     title = models.CharField(max_length=50, blank=True, default='')
+
+    def get_path(self) -> str:
+        return self.epub3_path
+    
+    def __str__(self) -> str:
+        return self.epub3_path
--- server/ebooks/views.py
+++ server/ebooks/views.py
@@ -1,9 +1,33 @@
 from rest_framework import viewsets
 from .serializers import EbookSerializer
 from .models import Ebook
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 
 class EbookView(viewsets.ModelViewSet):
     serializer_class = EbookSerializer
     queryset = Ebook.objects.all()
+
+    # This will (check if it exists in DB) then get the file from local storage, zip it to an epub and return to client
+    @action(detail=True, methods=["get"], url_path=r'download',)
+    def download(self, request, pk=None):
+        post = self.get_object() # the ebook object
+        print(post.get_path())
+        print(post)
+        serializer = self.get_serializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    # This will add the unzipped epub file into local storage, then create DB entry with uuid, title
+    @action(detail=False, methods=["post"], url_path=r'upload',)
+    def upload(self, request):
+        if request.method == 'POST':
+            # json_data = json.loads(request.body)
+            
+            return JsonResponse({'msg':'Upload GUD'})
+        return JsonResponse({'msg':'Upload BAD'})
