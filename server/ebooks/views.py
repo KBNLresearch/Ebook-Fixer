17240abfa442181693bd8df6654ff125593ae249
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2022-05-05 22:21:07 +0200

    Server unzips uploaded epub file under app/test-ebooks/{uuid}/{filename}

--- server/ebooks/views.py
+++ server/ebooks/views.py
@@ -1,11 +1,10 @@
-from xml.dom.minidom import Document
 from rest_framework import viewsets
 from .serializers import EbookSerializer
 from .models import Ebook
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
 from django.http import JsonResponse
+from rest_framework import status
 import zipfile
 import uuid 
 import os
@@ -16,6 +15,7 @@ import os
 class EbookView(viewsets.ModelViewSet):
     serializer_class = EbookSerializer
     queryset = Ebook.objects.all()
+    
 
     # This will (check if it exists in DB) then get the file from local storage, zip it to an epub and return to client
     @action(detail=True, methods=["get"], url_path=r'download',)
@@ -24,36 +24,37 @@ class EbookView(viewsets.ModelViewSet):
         print(post.get_path())
         print(post)
         serializer = self.get_serializer(post)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
+        return JsonResponse({'data': serializer.data}, status=status.HTTP_200_OK)
 
 
     @action(detail=False, methods=["post"], url_path=r'upload',)
     def upload(self, request):
-
-        print('\n\n\n ########################################## request.FILES:\n')  
-        print(request.FILES, '\n')              
-        print(request.FILES['epub'].file)  
-        print('\n########################################## \n\n\n')
-
+        # Generate random uuid for new ebook instance
         book_id = str(uuid.uuid4())
-        # binary_epub = request.FILES['epub'].file
+        uploaded_epub = request.FILES['epub']
+        binary_epub = request.FILES['epub'].file
         epub_name = request.FILES['epub'].name
         
-        # Check if file extension is  .epub
+        # Check if file extension is .epub
         file_ext = epub_name[-5:]
         if file_ext == '.epub':
-            # TODO: Extract title from HTML
-            new_ebook = Ebook(book_id, epub_name, request.FILES['epub'])
+            # TODO: Extract title from content.opf
+            new_ebook = Ebook(book_id, epub_name, uploaded_epub)
             new_ebook.save()
 
-            # TODO: Make accessible (Aratrika)
-                
-            # TODO: Return accessible epub file as response to client in Response
+            # Unzip the epub file stored on the server
+            with zipfile.ZipFile(f"/app/test-ebooks/{book_id}/{epub_name}", 'r') as zipped_epub:
+                zipped_epub.extractall(f"/app/test-ebooks/{book_id}")
 
-            return Response(data='<accessible ebook>', status=status.HTTP_200_OK)
+            # TODO: Make accessible (Aratrika)
+            # TODO: Convert epub2 to epub3 
+                
+            # TODO: Return accessible epub3 file (convert to JSON??) + bookid as response to client in Response
+
+            return JsonResponse({'data':f"New ebook entry created with uuid {book_id}"}, status=status.HTTP_200_OK)
+            # return JsonResponse({'data': binary_epub}, status=status.HTTP_200_OK)
         else: 
-            return Response(data='Make sure your uploaded file has extension .epub!', status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({'data':'Make sure your uploaded file has extension .epub!'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
