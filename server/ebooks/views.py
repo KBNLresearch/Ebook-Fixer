0e23764b02cb2f242b781836729eda8f233edcb3
Filip Nguyen Duc <H.C.NguyenDuc@student.tudelft.nl>
2022-05-06 17:58:02 +0200

    Added method descriptions and made download endpoint accept only GET

--- server/ebooks/views.py
+++ server/ebooks/views.py
@@ -11,6 +11,15 @@ import os
 
 
 def ebook_detail_view(request, uuid):
+    """The GET endpoint for an ebook instance
+
+    Args:
+        request (request object): The request object
+        uuid (str): The UUID of an already uploaded ebook
+
+    Returns:
+        JsonResponse: Response object sent to the client side
+    """
     if request.method == "GET":
         try:
             ebook = Ebook.objects.all().filter(uuid=uuid).get()
@@ -23,41 +32,54 @@ def ebook_detail_view(request, uuid):
 
 
 def ebook_download_view(request, uuid):
-    try:
-        ebook = Ebook.objects.all().filter(uuid=uuid).get()
-    except Ebook.DoesNotExist:
-        return JsonResponse({'msg': f'Ebook with uuid {uuid} not found!'},
-                            status=status.HTTP_404_NOT_FOUND)
-    images = Image.objects.all().filter(ebook=ebook).all()
-    annotations = [a for a in Annotation.objects.all()
-                   if a.image in images
-                   if a.type == 'HUM']
+    """Endpoint for zipping the ebook with given uuid from storage and reutrns the epub
 
-    # Get the html files from the storage
-    storage_path = f"test-books/{uuid}"
-    html_files = []
-    for folder_name, sub_folders, filenames in os.walk(storage_path):
-        for filename in filenames:
-            if filename.endswith(".html"):
-                html_files.append(filename)
+    Args:
+        request (request object): The request object
+        uuid (str): The UUID of an already uploaded ebook
 
-    # Inject image annotations into the html files
-    inject_image_annotations(uuid, html_files, images, annotations)
+    Returns:
+        JsonResponse: Response object sent to the client side
+    """
+    if request.method == "GET":
+        try:
+            ebook = Ebook.objects.all().filter(uuid=uuid).get()
+        except Ebook.DoesNotExist:
+            return JsonResponse({'msg': f'Ebook with uuid {uuid} not found!'},
+                                status=status.HTTP_404_NOT_FOUND)
+        images = Image.objects.all().filter(ebook=ebook).all()
+        annotations = [
+            a for a in Annotation.objects.all()
+            if a.image in images
+            if a.type == 'HUM'
+        ]
 
-    try:
-        # Zip contents
-        print(f"Zipping: {uuid}")
-        zip_file_name = zip_ebook(uuid)
+        # Get the html files from the storage
+        storage_path = f"test-books/{uuid}"
+        html_files = []
+        for folder_name, sub_folders, filenames in os.walk(storage_path):
+            for filename in filenames:
+                if filename.endswith(".html"):
+                    html_files.append(filename)
 
-        # Return zipped contents
-        with open(zip_file_name, 'rb') as file:
-            response = HttpResponse(file, content_type='application/epub+zip')
-            response['Content-Disposition'] = f'attachment; filename={zip_file_name}'
-            return response
-    except FileNotFoundError:
-        return JsonResponse({'msg': f'Files for ebook with uuid {uuid} not found! '
-                                    f'Zipping failed!'},
-                            status=status.HTTP_404_NOT_FOUND)
+        # Inject image annotations into the html files
+        inject_image_annotations(uuid, html_files, images, annotations)
+
+        try:
+            # Zip contents
+            print(f"Zipping ebook with uuid: {uuid}")
+            zip_file_name = zip_ebook(uuid)
+
+            # Return zipped contents
+            with open(zip_file_name, 'rb') as file:
+                response = HttpResponse(file, content_type='application/epub+zip')
+                response['Content-Disposition'] = f'attachment; filename={zip_file_name}'
+                return response
+        except FileNotFoundError:
+            return JsonResponse({'msg': f'Files for ebook with uuid {uuid} not found! '
+                                        f'Zipping failed!'},
+                                status=status.HTTP_404_NOT_FOUND)
+    return JsonResponse({'msg': 'Method Not Allowed!'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 def ebook_upload_view(request):
