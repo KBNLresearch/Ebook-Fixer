91f5c785eeb8e5846138809aa75767fbab884df6
Denis Tsvetkov <D.R.Tsvetkov@student.tudelft.nl>
2022-05-06 14:53:40 +0200

    Update download view to only consider GET requests

--- server/ebooks/views.py
+++ server/ebooks/views.py
@@ -23,41 +23,43 @@ def ebook_detail_view(request, uuid):
 
 
 def ebook_download_view(request, uuid):
-    try:
-        ebook = Ebook.objects.all().filter(uuid=uuid).get()
-    except Ebook.DoesNotExist:
-        return JsonResponse({'msg': f'Ebook with uuid {uuid} not found!'},
-                            status=status.HTTP_404_NOT_FOUND)
-    images = Image.objects.all().filter(ebook=ebook).all()
-    annotations = [a for a in Annotation.objects.all()
-                   if a.image in images
-                   if a.type == 'HUM']
+    if request.method == "GET":
+        try:
+            ebook = Ebook.objects.all().filter(uuid=uuid).get()
+        except Ebook.DoesNotExist:
+            return JsonResponse({'msg': f'Ebook with uuid {uuid} not found!'},
+                                status=status.HTTP_404_NOT_FOUND)
+        images = Image.objects.all().filter(ebook=ebook).all()
+        annotations = [a for a in Annotation.objects.all()
+                       if a.image in images
+                       if a.type == 'HUM']
 
-    # Get the html files from the storage
-    storage_path = f"test-books/{uuid}"
-    html_files = []
-    for folder_name, sub_folders, filenames in os.walk(storage_path):
-        for filename in filenames:
-            if filename.endswith(".html"):
-                html_files.append(filename)
+        # Get the html files from the storage
+        storage_path = f"test-books/{uuid}"
+        html_files = []
+        for folder_name, sub_folders, filenames in os.walk(storage_path):
+            for filename in filenames:
+                if filename.endswith(".html"):
+                    html_files.append(filename)
 
-    # Inject image annotations into the html files
-    inject_image_annotations(uuid, html_files, images, annotations)
+        # Inject image annotations into the html files
+        inject_image_annotations(uuid, html_files, images, annotations)
 
-    try:
-        # Zip contents
-        print(f"Zipping: {uuid}")
-        zip_file_name = zip_ebook(uuid)
+        try:
+            # Zip contents
+            print(f"Zipping: {uuid}")
+            zip_file_name = zip_ebook(uuid)
 
-        # Return zipped contents
-        with open(zip_file_name, 'rb') as file:
-            response = HttpResponse(file, content_type='application/epub+zip')
-            response['Content-Disposition'] = f'attachment; filename={zip_file_name}'
-            return response
-    except FileNotFoundError:
-        return JsonResponse({'msg': f'Files for ebook with uuid {uuid} not found! '
-                                    f'Zipping failed!'},
-                            status=status.HTTP_404_NOT_FOUND)
+            # Return zipped contents
+            with open(zip_file_name, 'rb') as file:
+                response = HttpResponse(file, content_type='application/epub+zip')
+                response['Content-Disposition'] = f'attachment; filename={zip_file_name}'
+                return response
+        except FileNotFoundError:
+            return JsonResponse({'msg': f'Files for ebook with uuid {uuid} not found! '
+                                        f'Zipping failed!'},
+                                status=status.HTTP_404_NOT_FOUND)
+    return JsonResponse({'msg': 'Method Not Allowed!'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 def ebook_upload_view(request):
