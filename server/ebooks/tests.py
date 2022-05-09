ad97515088474deeab1197f7d0ff707761be43f3
Filip Nguyen Duc <H.C.NguyenDuc@student.tudelft.nl>
2022-05-09 10:59:41 +0200

    Test folders are now removed from server-vol

--- server/ebooks/tests.py
+++ server/ebooks/tests.py
@@ -62,6 +62,8 @@ class ViewsTest(TestCase):
         expected_data = str(EbookSerializer(ebook).data).replace("'", '"')
         self.assertEqual(data, expected_data)
 
+        shutil.rmtree(os.path.abspath("./") + f"/test-books/{self.uuid}")
+
     def response_ebook_download_view(self):
         request = self.factory.get(f'/download/{self.uuid}/')
         request.user = self.user
@@ -83,6 +85,7 @@ class ViewsTest(TestCase):
         request.user = self.user
 
         response = ebook_download_view(request, self.uuid)
+
     def test_ebook_upload_view_405(self):
         request = self.factory.get('upload/')
         request.user = self.user
@@ -93,39 +96,34 @@ class ViewsTest(TestCase):
         self.assertEqual(response.status_code, 405)
         self.assertEqual(msg, b'{"msg": "Method Not Allowed!"}')
 
-    # def test_ebook_download_view_404_file_not_found(self):
-    #     #test_file = SimpleUploadedFile("", b"These are the file contents")
-    #     Ebook.objects.create(uuid=uuid4(), title="TEST_TITLE")
+    def test_ebook_download_view_404_file_not_found(self):
+        Ebook.objects.create(uuid=self.uuid, title="TEST_TITLE", epub=None)
 
-    #     response, msg = self.response_ebook_download_view()
+        response, msg = self.response_ebook_download_view()
 
-    #     self.assertEqual(response.status_code, 404)
-    #     expected_msg = '{"msg": ' + f'"Files for ebook with uuid {self.uuid} not found!'\
-    #         ' Zipping failed!"' + '}'
-    #     self.assertEqual(msg, bytes(expected_msg, 'utf-8'))
+        self.assertEqual(response.status_code, 404)
+        expected_msg = '{"msg": ' + f'"Files for ebook with uuid {self.uuid} not found!'\
+            ' Zipping failed!"' + '}'
+        self.assertEqual(msg, bytes(expected_msg, 'utf-8'))
 
-    # def test_ebook_download_view_200(self):
-    #     test_filename = "test_content.txt"
-    #     test_txt_content = b"Represents an unzipped ebook"
+    def test_ebook_download_view_200(self):
+        test_filename = "test_content.txt"
+        test_txt_content = b"Represents an unzipped ebook"
 
-    #     test_file = SimpleUploadedFile(test_filename, test_txt_content)
-    #     Ebook.objects.create(uuid=self.uuid, title="TEST_TITLE", epub=test_file)
+        test_file = SimpleUploadedFile(test_filename, test_txt_content)
+        Ebook.objects.create(uuid=self.uuid, title="TEST_TITLE", epub=test_file)
 
-    #     folder_path = os.path.abspath("./") + f"/test-books/{self.uuid}/"
-    #     os.mkdir(folder_path)
-    #     file_path = f"{folder_path}{test_filename}"
+        folder_path = os.path.abspath("./") + f"/test-books/{self.uuid}/"
+        file_path = f"{folder_path}{test_filename}"
 
-    #     with open(file_path, "w") as file:
-    #         file.write(test_txt_content)
+        response, msg = self.response_ebook_download_view()
 
-    #     response, msg = self.response_ebook_download_view()
+        epub_path = f"./{self.uuid}.epub"
 
-    #     epub_path = f"./{self.uuid}.epub"
+        self.assertTrue(os.path.exists(f"{epub_path}"))
+        self.assertEqual(response.status_code, 200)
 
-    #     self.assertTrue(os.path.exists(f"{epub_path}"))
-    #     self.assertEqual(response.status_code, 200)
-
-    #     shutil.rmtree(folder_path)
+        shutil.rmtree(folder_path)
 
     def test_ebook_upload_view_400(self):
         test_file = SimpleUploadedFile(
