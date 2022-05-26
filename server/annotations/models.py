521314a5ab730a845913099092bbabbdb6923de1
Filip Nguyen Duc <H.C.NguyenDuc@student.tudelft.nl>
2022-05-26 17:15:39 +0200

    Updates annotation model

--- server/annotations/models.py
+++ server/annotations/models.py
@@ -5,10 +5,12 @@ from images.models import Image
 class Annotation(models.Model):
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     ANNOTATION_TYPES = [
-        ('BB', 'Black-box'),
+        ('BB_GOOGLE_LAB', 'Black-box Google Label'),
+        ('BB_AZURE_LAB', 'Black-box Azure Label'),
+        ('BB_AZURE_SEN', 'Black-box Azure Sentence'),
         ('SMART', 'Smart AI'),
         ('HUM', 'Human')
     ]
-    type = models.CharField(max_length=10, choices=ANNOTATION_TYPES, default='BB')
-    text = models.CharField(max_length=200, default="")
+    type = models.CharField(max_length=15, choices=ANNOTATION_TYPES, default='HUM')
+    text = models.CharField(max_length=200)
     confidence = models.DecimalField(max_digits=5, decimal_places=4, default=1.0)
