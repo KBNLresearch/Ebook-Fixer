92db180859973245d867aa66808a6b959fcd5437
Vlad Makarov <v.makarov@student.tudelft.nl>
2022-05-27 19:36:26 +0200

    Finish client docs

--- client/src/components/editor/Viewer.js
+++ client/src/components/editor/Viewer.js
@@ -5,7 +5,8 @@ import styles from './Editor.module.scss'
  * The Viewer component contains the div that holds the rendering of the Ebook
  * Right now, it just needs to create a div with a certain id so that EpubJS can use it to render the ebook.
  *
- * @param {{id: String}} props id of the div
+ * @param {String} id id of the div to display the book in
+ * @component
  * @returns The Viewer component
  */
 function Viewer({ id }) {
