96b8d53853f4bb82c19e5164aaa8f79912ccc33f
Vlad Makarov <v.makarov@student.tudelft.nl>
2022-05-28 12:47:26 +0200

    Generate client docs

---
+++ client-docs/entry.js
@@ -0,0 +1,59 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../client/node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../client/src/components/editor/AIAnnotator.js';
+reactComponents['AIAnnotator'] = Component0;
+
+import Component1 from '../client/src/components/editor/Annotator.js';
+reactComponents['Annotator'] = Component1;
+
+import Component2 from '../client/src/App.js';
+reactComponents['App'] = Component2;
+
+import Component3 from '../client/src/components/editor/Classifier.js';
+reactComponents['Classifier'] = Component3;
+
+import Component4 from '../client/src/components/editor/Editor.js';
+reactComponents['Editor'] = Component4;
+
+import Component5 from '../client/src/components/editor/EditorControls.js';
+reactComponents['EditorControls'] = Component5;
+
+import Component6 from '../client/src/components/EpubInfoPage.js';
+reactComponents['EpubInfoPage'] = Component6;
+
+import Component7 from '../client/src/components/FileDownload.js';
+reactComponents['FileDownload'] = Component7;
+
+import Component8 from '../client/src/components/FileUpload.js';
+reactComponents['FileUpload'] = Component8;
+
+import Component9 from '../client/src/components/editor/ShareURL.js';
+reactComponents['ShareURL'] = Component9;
+
+import Component10 from '../client/src/components/editor/Annotator.js';
+reactComponents['Annotator'] = Component10;
+
+import Component11 from '../client/src/components/editor/Viewer.js';
+reactComponents['Viewer'] = Component11;
