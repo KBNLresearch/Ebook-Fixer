f825f0cba9a4f860955fe6e8c19a1ccefa51a280
Denis <D.R.Tsvetkov@student.tudelft.nl>
2022-05-28 16:59:22 +0200

    Set up client documentation volume

--- docs/client/entry.js
+++ docs/client/entry.js
@@ -9,7 +9,7 @@
       import ReactDOM from "react-dom";
 
 
-      import ReactWrapper from '../../client/node_modules/better-docs/lib/react-wrapper.js';
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
 
       window.React = React;
 
@@ -22,38 +22,38 @@
 
     import './styles/iframe.css';
 
-  import Component0 from '../../client/src/components/editor/AIAnnotator.js';
+  import Component0 from '../src/components/editor/AIAnnotator.js';
 reactComponents['AIAnnotator'] = Component0;
 
-import Component1 from '../../client/src/components/editor/Annotator.js';
+import Component1 from '../src/components/editor/Annotator.js';
 reactComponents['Annotator'] = Component1;
 
-import Component2 from '../../client/src/App.js';
+import Component2 from '../src/App.js';
 reactComponents['App'] = Component2;
 
-import Component3 from '../../client/src/components/editor/Classifier.js';
+import Component3 from '../src/components/editor/Classifier.js';
 reactComponents['Classifier'] = Component3;
 
-import Component4 from '../../client/src/components/editor/Editor.js';
+import Component4 from '../src/components/editor/Editor.js';
 reactComponents['Editor'] = Component4;
 
-import Component5 from '../../client/src/components/editor/EditorControls.js';
+import Component5 from '../src/components/editor/EditorControls.js';
 reactComponents['EditorControls'] = Component5;
 
-import Component6 from '../../client/src/components/EpubInfoPage.js';
+import Component6 from '../src/components/EpubInfoPage.js';
 reactComponents['EpubInfoPage'] = Component6;
 
-import Component7 from '../../client/src/components/FileDownload.js';
+import Component7 from '../src/components/FileDownload.js';
 reactComponents['FileDownload'] = Component7;
 
-import Component8 from '../../client/src/components/FileUpload.js';
+import Component8 from '../src/components/FileUpload.js';
 reactComponents['FileUpload'] = Component8;
 
-import Component9 from '../../client/src/components/editor/ShareURL.js';
+import Component9 from '../src/components/editor/ShareURL.js';
 reactComponents['ShareURL'] = Component9;
 
-import Component10 from '../../client/src/components/editor/Annotator.js';
+import Component10 from '../src/components/editor/Annotator.js';
 reactComponents['Annotator'] = Component10;
 
-import Component11 from '../../client/src/components/editor/Viewer.js';
+import Component11 from '../src/components/editor/Viewer.js';
 reactComponents['Viewer'] = Component11;
