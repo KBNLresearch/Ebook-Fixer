7cd697c567b499b1f387067717a6faac64edf99c
Denis <D.R.Tsvetkov@student.tudelft.nl>
2022-05-29 19:24:26 +0200

    Fix automatic deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,17 +11,26 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from os import environ
-import os
+from os import environ, path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/test-books/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'test-books')
+MEDIA_ROOT = path.join(BASE_DIR, 'test-books')
 
-# For development
-if environ.get('GOOGLE_APPLICATION_CREDENTIALS') is None:
-    environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./arcane-pillar-349913-6276b9f35040.json"
+# For development:
+google_credentials = environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+if google_credentials is None:
+    environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_credentials = "./arcane-pillar-349913-6276b9f35040.json"
+
+# For automatic deployment:
+if not path.isfile(google_credentials):
+    with open(google_credentials, 'w') as file:
+        file.write(environ.get('GOOGLE_CREDENTIALS'))
+certificate = environ.get('MONGO_DB_CA_CERT')
+if certificate is not None and not path.isfile(certificate):
+    with open(certificate, 'w') as file:
+        file.write(environ.get('CA_CERTIFICATE'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
