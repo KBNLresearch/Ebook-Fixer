6b12e07da0f9b13bff117f97b405b173d2a3004c
Vlad Makarov <v.makarov@student.tudelft.nl>
2022-06-03 14:40:50 +0200

    Add docs

--- client/src/helpers/EbookContext.js
+++ client/src/helpers/EbookContext.js
@@ -1,3 +1,11 @@
 import { atom } from 'jotai'
 
+// This file contains context that will be available globally for all components to use.
+// For a tutorial on using jotai: https://jotai.org/ (it's pretty intuitive)
+
+/**
+ * Will be set to the function for changing to the next image.
+ * null if there is no next image
+ * @private
+ */
 export const nextImage = atom(null)
